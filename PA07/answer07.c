f33a6072af46619894693f3eed646f075c0e4018
Willis R Banks <banksw@ecelinux42.ecn.purdue.edu>
2013-10-22 17:28:38 -0400

    All bugs have been splatted, though at the cost of hair.

--- PA07/answer07.c
+++ PA07/answer07.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void List_modify(Node * head, int value,int index);
+
 /**
  * Prints a linked-list "head" into the output fie "out"
  *
@@ -35,7 +37,28 @@ void List_print(FILE * out, Node * head)
  */
 void List_destroy(Node * head)
 {
-
+  if(head==NULL)
+    {
+      return;
+    }
+  struct linked * previous = head;
+  struct linked * target = head;
+  if(head->next !=NULL)
+    {
+      target=target->next;
+      while(target!=NULL)
+	{
+	  free(previous);
+	  previous=target;
+	  target=target->next;
+	}
+      free(previous);
+    }
+  else
+    {
+      free(head);
+    }
+  return;
 }
 
 /**
@@ -53,8 +76,11 @@ void List_destroy(Node * head)
  */
 Node * List_create(int value, int index)
 {
-
-    return NULL;
+  struct linked * newnode =  malloc(sizeof(struct linked));
+  newnode->value=value;
+  newnode->index=index;
+  newnode->next=NULL;
+  return(newnode);  
 }
 
 /**
@@ -85,7 +111,51 @@ Node * List_create(int value, int index)
  */
 Node * List_build(int * value, int * index, int length)
 {
-    return NULL;
+  if(length==0)
+    {
+      return(NULL);
+    }
+  int small=index[0];
+  int smalloc=0;
+  int big=index[0];
+  int bigloc=0;
+  int i;
+  struct linked * list;
+  struct linked * ret;
+
+  for(i=0;i<length;i++)
+    {
+      if(index[i]<small)
+	{
+	  small=index[i];
+	  smalloc=i;
+	}
+      if(index[i]>big)
+	{
+	  big=index[i];
+	  bigloc=i;
+	}
+    }
+  
+  list=List_create(value[smalloc],index[smalloc]);
+  ret=list;
+
+  while(small!=big)
+    {
+      small=big;
+      smalloc=bigloc;
+      for(i=0;i<length;i++)
+	{
+	  if(index[i]>list->index && index[i]<small)
+	    {
+	      small=index[i];
+	      smalloc=i;
+	    }
+	}
+      list->next=List_create(value[smalloc],index[smalloc]);
+      list=list->next;
+    }
+  return(ret);
 }
 
 
@@ -110,7 +180,33 @@ Node * List_build(int * value, int * index, int length)
  */
 Node * List_insert_ascend(Node * head, int value, int index)
 {
-    return NULL;
+  if(index<head->index)
+    {
+      struct linked * new=List_create(value,index);
+      new->next=head;
+      return(new);
+    }
+  struct linked * prev=head;
+  struct linked * point=prev->next;
+  struct linked * newnode = List_create(value,index);
+
+  //Shift down to appropriate location
+  while(point->index<index && point->next!=NULL)
+    {
+      prev=prev->next;
+      point=point->next;
+    }
+  //Make attachments
+  if(point->index < index)
+    {
+      point->next=newnode;
+    }
+  else
+    {
+      prev->next=newnode;
+      newnode->next=point;
+    }
+  return(head);
 }
 
 
@@ -126,7 +222,26 @@ Node * List_insert_ascend(Node * head, int value, int index)
  */
 Node * List_delete(Node * head, int index)
 {
-    return NULL;
+  struct linked * target = head->next;
+  struct linked * prev=head;
+  if(prev->index==index)
+    {
+      free(prev);
+      return(target);
+    }
+  //Move pointers to target and previous values
+  while(target->index!=index)
+    {
+      prev=prev->next;
+      target=target->next;
+    }
+  //Reattach next value and free the target
+  if(target->next!=NULL)
+    {
+      prev->next=target->next;
+    }
+  free(target);
+  return(head);
 }
 
 /**
@@ -148,7 +263,22 @@ Node * List_delete(Node * head, int index)
  */
 Node * List_copy(Node * head)
 {
-    return NULL;
+  if(head==NULL)
+    {
+      return(NULL);
+    }
+
+  struct linked * target = head;
+  struct linked * copy=List_create(target->value,target->index);
+  struct linked * copy_head=copy;
+
+  while(target->next!=NULL)
+    {
+      target=target->next;
+      copy->next=List_create(target->value,target->index);
+      copy=copy->next;
+    }
+  return(copy_head);
 }
 
 
@@ -174,6 +304,108 @@ Node * List_copy(Node * head)
  */
 Node * List_merge(Node * head1, Node * head2)
 {
-    return NULL;
+  if(head1==NULL && head2==NULL)
+    {
+      return(NULL);
+    }
+  else if(head1==NULL)
+    {
+      return(List_copy(head2));
+    }
+  else if(head2==NULL)
+    {
+      return(List_copy(head1));
+    }
+
+  struct linked * merge=List_copy(head1);
+  struct linked * list1=head1;
+  struct linked * list2=head2;
+  int ind;
+  int val;
+
+  if(head1->next==NULL && head2->next==NULL)
+    {
+      if(head1->index != head2->index)
+	{
+	  merge=List_insert_ascend(merge,head2->value,head2->index);
+	}
+      else
+	{
+	  List_destroy(merge);
+	  merge=List_create(head1->value+head2->value,head1->index);
+	}
+      return(merge);
+    }
+  
+  if(head1->next==NULL)
+    {
+      List_destroy(merge);
+      merge=List_copy(head2);
+      if(head1->index==head2->index && head1->value+head2->value==0)
+	{
+	  merge=List_delete(merge,head1->index);
+	  return(merge);
+	}
+      else if(head1->index==head2->index)
+	{
+	  List_modify(merge,head1->value+head2->value,head1->index);
+	  return(merge);
+	}
+      else
+	{
+	  merge=List_insert_ascend(merge,head1->value,head1->index);;
+	  return(merge);
+	}
+    }
+
+  while(list2 != NULL)
+    {
+      if(list1->next==NULL && list1->index<list2->index)
+	{
+	  ind=list2->index;
+	  val=list2->value;
+	  list2=list2->next;
+	  merge=List_insert_ascend(merge,val,ind);
+	}
+      else if(list1->index < list2->index)
+	{
+	  list1=list1->next;
+	}
+      else if(list1->index > list2->index)
+	{
+	  ind=list2->index;
+	  val=list2->value;
+	  list2=list2->next;
+	  merge=List_insert_ascend(merge,val,ind);
+	}
+      else if(list1->index == list2->index)
+	{
+	  ind=list1->index;
+	  val=list1->value+list2->value;
+	  if(list1->next!=NULL)
+	    {
+	      list1=list1->next;
+	    }
+	  list2=list2->next;
+	  if(val!=0)
+	    {
+	      List_modify(merge,val,ind);
+	    }
+	  else
+	    {
+	      merge=List_delete(merge,ind);
+	    }
+	}
+    }  
+  return(merge);
 }
 
+void List_modify(Node * head,int value,int index)
+{
+  while(head->index!=index)
+    {
+      head=head->next;
+    }
+  head->value=value;
+  return;
+}
